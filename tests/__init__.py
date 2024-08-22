4b955f9b87438399a2155766217372f77e5faa16
Subhas Bhat <bhatsubhas@gmail.com>
2024-08-22 11:18:59 +0530

    fix issue: fixture 'client' not found

--- tests/__init__.py
+++
@@ -1,8 +0,0 @@
-import pytest
-from app import app
-
-
-@pytest.fixture(scope="module")
-def client():
-    with app.test_client() as flask_client:
-        yield flask_client
---
+++ tests/conftest.py
@@ -0,0 +1,8 @@
+import pytest
+from app import app
+
+
+@pytest.fixture(scope="module")
+def client():
+    with app.test_client() as flask_client:
+        yield flask_client
