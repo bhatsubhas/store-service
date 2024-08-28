f9c0012a6241776bd5c41b214fff959f4baef344
Subhas Bhat <bhatsubhas@gmail.com>
2024-08-28 13:29:13 +0530

    new endpoints and code refactor

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,6 @@
 import uuid
 from flask import Flask, request
+from flask_smorest import abort
 from .db import stores, items
 
 
@@ -19,6 +20,12 @@ def get_stores():
 @app.post("/stores")
 def create_stores():
     store_data = request.get_json()
+    if "name" not in store_data:
+        abort(400, error="'name' is mandatory")
+
+    for store in stores.values():
+        if store["name"] == store_data["name"]:
+            abort(400, error="Store already exists")
     store_id = uuid.uuid4().hex
     store = {**store_data, "store_id": store_id}
     stores[store_id] = store
@@ -28,8 +35,22 @@ def create_stores():
 @app.post("/items")
 def create_item():
     item_data = request.get_json()
+    if (
+        "price" not in item_data
+        or "name" not in item_data
+        or "store_id" not in item_data
+    ):
+        abort(400, error="'price', 'name' and 'store_id' are mandatory")
+
+    for item in items.values():
+        if (
+            item["name"] == item_data["name"]
+            and item["store_id"] == item_data["store_id"]
+        ):
+            abort(400, error="Item already exists")
+
     if item_data["store_id"] not in stores:
-        return {"error": "STORE_NOT_FOUND"}, 404
+        abort(404, error="Store not found")
 
     item_id = uuid.uuid4().hex
     item = {**item_data, "item_id": item_id}
@@ -42,7 +63,30 @@ def get_store(store_id):
     try:
         return stores[store_id]
     except KeyError:
-        return {"error": "STORE_NOT_FOUND"}, 404
+        abort(404, error="Store not found")
+
+
+@app.patch("/stores/<string:store_id>")
+def update_store(store_id):
+    store_data = request.get_json()
+    if "name" not in store_data:
+        abort(400, "'name' must be present")
+
+    try:
+        store = stores[store_id]
+        store |= store_data
+        return store
+    except KeyError:
+        abort(404, error="Store not found")
+
+
+@app.delete("/stores/<string:store_id>")
+def delete_store(store_id):
+    try:
+        del stores[store_id]
+        return "", 204
+    except KeyError:
+        abort(404, error="Store not found")
 
 
 @app.get("/items")
@@ -55,4 +99,27 @@ def get_item(item_id):
     try:
         return items[item_id]
     except KeyError:
-        return {"error": "ITEM_NOT_FOUND"}, 404
+        abort(404, error="Item not found")
+
+
+@app.patch("/items/<string:item_id>")
+def udpate_item(item_id):
+    item_data = request.get_json()
+    if "name" not in item_data or "price" not in item_data:
+        abort(400, "Either 'name' or 'price' must be present")
+
+    try:
+        item = items[item_id]
+        item |= item_data
+        return item
+    except KeyError:
+        abort(404, error="Item not found")
+
+
+@app.delete("/items/<string:item_id>")
+def delete_item(item_id):
+    try:
+        del items[item_id]
+        return "", 204
+    except KeyError:
+        abort(404, error="Item not found")
--- tests/test_stores.py
+++ tests/test_stores.py
@@ -19,8 +19,8 @@ def test_get_store(client):
     response = client.get(f"/stores/{store_id}")
     assert response.status_code == 404
 
-    error = response.get_json()
-    assert error["error"] == "STORE_NOT_FOUND"
+    # error = response.get_json()
+    # assert error["error"] == "STORE_NOT_FOUND"
 
 
 def test_create_store(client):
@@ -31,3 +31,41 @@ def test_create_store(client):
     store = response.get_json()
     assert store["name"] == "Test Store"
     assert store["store_id"] is not None
+
+    request_body = {}
+    response = client.post("/stores", json=request_body)
+    assert response.status_code == 400
+
+    request_body = {"name": "My Store"}
+    response = client.post("/stores", json=request_body)
+    assert response.status_code == 400
+
+
+def test_update_store(client):
+    store_id = "1"
+    store_data = {"name": "My Premium Store"}
+    response = client.patch(f"/stores/{store_id}", json=store_data)
+    assert response.status_code == 200
+
+    item = response.get_json()
+    assert item["store_id"] == "1" and item["name"] == "My Premium Store"
+
+    store_id = "1"
+    store_data = {}
+    response = client.patch(f"/stores/{store_id}", json=store_data)
+    assert response.status_code == 400
+
+    store_id = "1234"
+    store_data = {"name": "My Premium Store"}
+    response = client.patch(f"/stores/{store_id}", json=store_data)
+    assert response.status_code == 404
+
+
+def test_delete_store(client):
+    store_id = "1"
+    response = client.delete(f"/stores/{store_id}")
+    assert response.status_code == 204
+
+    store_id = "1234"
+    response = client.delete(f"/stores/{store_id}")
+    assert response.status_code == 404
